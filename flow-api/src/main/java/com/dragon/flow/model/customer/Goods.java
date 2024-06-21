132e7cc825c3ccaa7dd83c54923453a3774b7951
lichen <lc@126.com>
2024-06-21 18:02:47 +0800

    解决了有事刷新不及时的问题

--- flow-api/src/main/java/com/dragon/flow/model/customer/Goods.java
+++ flow-api/src/main/java/com/dragon/flow/model/customer/Goods.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.dragon.tools.common.BaseModel;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -60,9 +61,11 @@ public class Goods extends BaseModel implements Serializable {
     /**
      * 上架时间
      */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private Date startTime;
     /**
      * 下架时间
      */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private Date endTime;
 }
