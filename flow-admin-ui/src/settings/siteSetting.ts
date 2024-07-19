ff917019f1dfe56780665cb08740cd5131c7c552
lichen <lc@126.com>
2024-07-19 10:43:39 +0800

    首页开发

--- flow-admin-ui/src/settings/siteSetting.ts
+++
@@ -1,8 +0,0 @@
-// github repo url
-export const GITHUB_URL = 'https://gitee.com/lwj/flow';
-
-// vue-vben-admin-next-doc
-export const DOC_URL = 'https://gitee.com/lwj/flow';
-
-// site url
-export const SITE_URL = 'https://gitee.com/lwj/flow';
--- flow-front-ui/src/views/process-form/index.ts
+++
@@ -1,8 +0,0 @@
-import Leave from './leave/index.vue';
-import Dimission from './dimission/index.vue';
-
-
-export default {
-  Leave,
-  Dimission
-}
